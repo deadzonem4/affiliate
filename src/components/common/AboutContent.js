4b3b9b57fe3986de785423ab21f95ff4a4801218
Dev <adelina@Adelinas-MacBook-Pro-2.local>
2019-01-11 14:20:32 +0200

    About us page logic

---
+++ src/components/common/AboutContent.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './styles/main.css';
+
+
+const AboutContent = (props) => {
+  return (
+    <div className="container">
+      <div className="about-content">
+        <h2 className="dark-title">{props.api[0][props.title]}</h2>
+        <div className="title-red-line"></div>
+        <div className="about-text">
+          <div dangerouslySetInnerHTML={{__html: props.api[0][props.content]}} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AboutContent;
+
