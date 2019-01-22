5cae6a420598c3c2fc5864158d6abeb5803cdf33
Dev <adelina@Adelinas-MacBook-Pro-2.local>
2019-01-22 14:57:13 +0200

    Latest news logic added

--- src/templates/en/LatestNews.js
+++ src/templates/en/LatestNews.js
@@ -2,37 +2,29 @@ import React from "react";
 import {Link} from "react-router-dom";
 import '../styles/main.css';
 
-class LatestNews extends React.Component {
-
-  render() {
-    var length = this.props.data.length;
-
-    return (
-      <div className="single-article-sidebar">
-        <Link  to={{pathname: `/article${length}`}}>
-          <div className="single-sidebar-article">
-            <span>{this.props.data[length-1].date}</span>
-            <h4>{this.props.data[length-1].subtitle_en}</h4>
-          </div>
-        </Link>
-        <Link  to={{pathname: `/article${length-1}`}}>
-          <div className="single-sidebar-article">
-            <span>{this.props.data[length-2].date}</span>
-            <h4>{this.props.data[length-2].subtitle_en}</h4>
-          </div>
-        </Link>
-        <Link  to={{pathname: `/article${length-2}`}}>
-          <div className="single-sidebar-article">
-            <span>{this.props.data[length-3].date}</span>
-            <h4>{this.props.data[length-3].subtitle_en}</h4>
-          </div>
-        </Link>
-      </div>
-    );
-  }
+const LatestNews = (props) => {
+  return (
+    <div className="single-article-sidebar">
+      <Link  to={{pathname: `/article${1}`}}>
+        <div className="single-sidebar-article">
+          <span>{props.data[0].date}</span>
+          <h4>{props.data[0].title_en}</h4>
+        </div>
+      </Link>
+      <Link  to={{pathname: `/article${2}`}}>
+        <div className="single-sidebar-article">
+          <span>{props.data[1].date}</span>
+          <h4>{props.data[1].title_en}</h4>
+        </div>
+      </Link>
+      <Link  to={{pathname: `/article${3}`}}>
+        <div className="single-sidebar-article">
+          <span>{props.data[2].date}</span>
+          <h4>{props.data[2].title_en}</h4>
+        </div>
+      </Link>
+    </div>
+  );
 }
 
-export default LatestNews;
-
-
-
+export default LatestNews
